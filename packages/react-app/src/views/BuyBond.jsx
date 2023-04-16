bdbebac4f20e0ff75bf9189af62f4c540958366e
Matthew Choi <matthew.choi02@gmail.com>
2023-04-16 08:04:55 -0500

    added an approve button

--- packages/react-app/src/views/BuyBond.jsx
+++ packages/react-app/src/views/BuyBond.jsx
@@ -72,6 +72,141 @@ function BuyBond({ userSigner, deals }) {
   const [bondsToBuy, setBondsToBuy] = useState(0);
   const [usdcDeposited, setUsdcDeposited] = useState(false);
   const [isSuccessModalOpen, setIsSuccessModalOpen] = useState(false);
+  const [usdcApproved, setUsdcApproved] = useState(false);
+
+  const handleApproveUSDC = async () => {
+    // Check if the value in the bondsToBuy input is an integer greater than 0
+    if (bondsToBuy <= 0 || !Number.isInteger(bondsToBuy)) {
+      alert("Please enter a valid number of bonds greater than 0.");
+      return;
+    }
+    const usdc = "0xA6847a9b6b6aF5bd09A36c3b331f6c66CA46c998";
+    const usdcABI = [
+      { inputs: [], stateMutability: "nonpayable", type: "constructor" },
+      {
+        anonymous: false,
+        inputs: [
+          { indexed: true, internalType: "address", name: "owner", type: "address" },
+          { indexed: true, internalType: "address", name: "spender", type: "address" },
+          { indexed: false, internalType: "uint256", name: "value", type: "uint256" },
+        ],
+        name: "Approval",
+        type: "event",
+      },
+      {
+        inputs: [
+          { internalType: "address", name: "spender", type: "address" },
+          { internalType: "uint256", name: "amount", type: "uint256" },
+        ],
+        name: "approve",
+        outputs: [{ internalType: "bool", name: "", type: "bool" }],
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+      {
+        inputs: [
+          { internalType: "address", name: "spender", type: "address" },
+          { internalType: "uint256", name: "subtractedValue", type: "uint256" },
+        ],
+        name: "decreaseAllowance",
+        outputs: [{ internalType: "bool", name: "", type: "bool" }],
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+      {
+        inputs: [
+          { internalType: "address", name: "spender", type: "address" },
+          { internalType: "uint256", name: "addedValue", type: "uint256" },
+        ],
+        name: "increaseAllowance",
+        outputs: [{ internalType: "bool", name: "", type: "bool" }],
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+      {
+        inputs: [
+          { internalType: "address", name: "to", type: "address" },
+          { internalType: "uint256", name: "amount", type: "uint256" },
+        ],
+        name: "transfer",
+        outputs: [{ internalType: "bool", name: "", type: "bool" }],
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+      {
+        anonymous: false,
+        inputs: [
+          { indexed: true, internalType: "address", name: "from", type: "address" },
+          { indexed: true, internalType: "address", name: "to", type: "address" },
+          { indexed: false, internalType: "uint256", name: "value", type: "uint256" },
+        ],
+        name: "Transfer",
+        type: "event",
+      },
+      {
+        inputs: [
+          { internalType: "address", name: "from", type: "address" },
+          { internalType: "address", name: "to", type: "address" },
+          { internalType: "uint256", name: "amount", type: "uint256" },
+        ],
+        name: "transferFrom",
+        outputs: [{ internalType: "bool", name: "", type: "bool" }],
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+      {
+        inputs: [
+          { internalType: "address", name: "owner", type: "address" },
+          { internalType: "address", name: "spender", type: "address" },
+        ],
+        name: "allowance",
+        outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [{ internalType: "address", name: "account", type: "address" }],
+        name: "balanceOf",
+        outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "decimals",
+        outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "name",
+        outputs: [{ internalType: "string", name: "", type: "string" }],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "symbol",
+        outputs: [{ internalType: "string", name: "", type: "string" }],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "totalSupply",
+        outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+        stateMutability: "view",
+        type: "function",
+      },
+    ];
+    const usdcContract = new ethers.Contract(usdc, usdcABI, userSigner);
+
+    console.log("Approving USDC to send to deal contract...");
+    const tx1 = await usdcContract.approve(dealContract.address, await dealContract.calcAtomicPrice(bondsToBuy));
+    console.log("USDC approved.");
+    setUsdcApproved(true);
+  };
 
   const handleDepositUSDC = async () => {
     // Check if the value in the bondsToBuy input is an integer greater than 0
@@ -124,9 +259,16 @@ function BuyBond({ userSigner, deals }) {
       </div>
       <br />
       <button
-        style={{ ...styles.button, ...(usdcDeposited ? styles.buttonDisabled : {}) }}
+        style={{ ...styles.button, ...(usdcApproved ? styles.buttonDisabled : {}) }}
+        onClick={handleApproveUSDC}
+        disabled={usdcApproved}
+      >
+        Approve USDC
+      </button>
+      <button
+        style={{ ...styles.button, ...(!usdcApproved ? styles.buttonDisabled : {}) }}
         onClick={handleDepositUSDC}
-        disabled={usdcDeposited}
+        disabled={!usdcApproved}
       >
         Deposit USDC
       </button>
