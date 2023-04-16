80cb47897a7010f8503952e8f43e9c3c9c2232ac
Brian <80086776+brianeide@users.noreply.github.com>
2023-04-15 21:52:45 -0400

    clean

--- packages/react-app/src/views/CreateDeal.jsx
+++ packages/react-app/src/views/CreateDeal.jsx
@@ -1,12 +1,6 @@
 import React from "react";
 import { BondForm } from "../components";
 
-/**
- * web3 props can be passed from '../App.jsx' into your local view component for use
- * @param {*} yourLocalBalance balance on current network
- * @param {*} readContracts contracts from current chain already pre-loaded using ethers contract module. More here https://docs.ethers.io/v5/api/contract/contract/
- * @returns react component
- **/
 function CreateDeal({ contractAddress, userSigner }) {
   return (
     <div>
